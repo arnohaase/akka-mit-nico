4ec89cad4c6a3b791eb12625a19d75c9fe6e793e
Arno Haase <arno.haase@haase-consulting.com>
2015-06-26 00:15:42 +0200

    progress

--- akka-hello/src/main/scala/de/arnohaase/sysinfo/infoProvider.scala
+++ akka-hello/src/main/scala/de/arnohaase/sysinfo/infoProvider.scala
@@ -1,18 +1,61 @@
 package de.arnohaase.sysinfo
 
-import akka.actor.Actor
+import akka.actor.{Props, ActorRef, Actor}
 import akka.actor.Actor.Receive
+import akka.actor.FSM.Failure
+import akka.pattern.Patterns
+
+import scala.concurrent.duration.FiniteDuration
+import scala.util.Success
+import scala.concurrent.duration._
 
 /**
  * @author arno
  */
 
 case object GiveMeData
+case object Finished
+case class SystemDataRequest (client: ActorRef, timeout: FiniteDuration)
 
-class InfoProviderActor extends Actor {
+class InfoProviderActor (scalarProviders: List[ActorRef]) extends Actor { //TODO dynamic (un)registration of scalar providers?
+  override def receive = {
+    case GiveMeData =>
+      val client = sender()
+      val props = Props(new InfoDataCollector (client, scalarProviders, 1 seconds)) //TODO clean up timeout
+      context.system.actorOf (props)
+  }
+}
+
+class InfoDataCollector (client: ActorRef, scalarProviders: List[ActorRef], timeout: FiniteDuration) extends Actor {
+  var numResponses = 0
+  var result = Map[ScalarType, ScalarValue]()
+
+  scalarProviders.foreach (_ ! GiveMeData)
+
+  val timeoutHandle = context.system.scheduler.scheduleOnce(timeout, context.self, Finished)(context.system.dispatcher)
+
+  override def receive = {
+    case m: Map[ScalarType, ScalarValue] =>
+      println ("received partial data: " + m)
+      result ++= m
+      numResponses += 1
+      if (numResponses == scalarProviders.size) {
+        timeoutHandle.cancel()
+        self ! Finished
+      }
+    case Finished =>
+      println ("finished - sending response to " + client)
+      client ! result
+      context.stop(self)
+  }
+}
+
+class CurrentTimeActor extends Actor {
   val curTimeType = ScalarType ("Current time", "ms", 0)
 
   override def receive = {
-    case GiveMeData => sender ! Map (curTimeType -> ScalarValue (System.currentTimeMillis(), curTimeType))
+    case GiveMeData =>
+      println ("received request for time data")
+      sender ! Map (curTimeType -> ScalarValue (System.currentTimeMillis(), curTimeType))
   }
 }
